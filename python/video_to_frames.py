8b68ace3b9bcf4aaf35a54accc962553f16dbbf7
Milan Košťák <milankostak@seznam.cz>
2020-03-06 22:46:33 +0100

    video_to_frames.py - saving info to text file

--- python/video_to_frames.py
+++ python/video_to_frames.py
@@ -4,13 +4,23 @@ capture = cv2.VideoCapture('input.mp4')
 success, image = capture.read()
 count = 0
 name = 0
+test_txt = ""
 frames_count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
 while success:
-    cv2.imwrite("%03d.jpg" % name, image)  # save frame as JPEG file
+    filename = "%03d.jpg" % name
     name += 1
+    cv2.imwrite(filename, image)  # save frame as JPEG file
+
+    row = ','.join(str(e) for e in [filename, -1, -1])
+    test_txt += row + "\n"
+
     success, image = capture.read()
     print('Read a new frame: ', success)
+
     count += 10
     capture.set(cv2.CAP_PROP_POS_FRAMES, count)
     if frames_count < count:
         success = False
+
+with open("test.txt", "w") as file:
+    file.write(test_txt)
