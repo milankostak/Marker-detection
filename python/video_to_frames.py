7618089287382896354fe5dde44d3b334f59b80b
Milan Košťák <milankostak@seznam.cz>
2020-03-07 11:13:00 +0100

    video_to_frames.py - load multiple videos

--- python/video_to_frames.py
+++ python/video_to_frames.py
@@ -1,26 +1,30 @@
 import cv2
+import glob
 
-capture = cv2.VideoCapture('input.mp4')
-success, image = capture.read()
-count = 0
+videos = glob.glob("input*.mp4")
 name = 0
 test_txt = ""
-frames_count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
-while success:
-    filename = "%03d.jpg" % name
-    name += 1
-    cv2.imwrite(filename, image)  # save frame as JPEG file
 
-    row = ','.join(str(e) for e in [filename, -1, -1])
-    test_txt += row + "\n"
-
+for video in videos:
+    capture = cv2.VideoCapture(video)
     success, image = capture.read()
-    print('Read a new frame: ', success)
+    count = 0
+    frames_count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+    while success:
+        filename = "%03d.jpg" % name
+        name += 1
+        cv2.imwrite(filename, image)  # save frame as JPEG file
 
-    count += 10
-    capture.set(cv2.CAP_PROP_POS_FRAMES, count)
-    if frames_count < count:
-        success = False
+        row = ','.join(str(e) for e in [filename, -1, -1])
+        test_txt += row + "\n"
+
+        success, image = capture.read()
+        print('Read a new frame: ', success)
+
+        count += 10
+        capture.set(cv2.CAP_PROP_POS_FRAMES, count)
+        if frames_count < count:
+            success = False
 
 with open("test.txt", "w") as file:
     file.write(test_txt)
