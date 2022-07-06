349c9c4c264f62da8df8eeb8b110a27d264882b7
Milan Košťák <milankostak@seznam.cz>
2022-07-06 14:44:42 +0200

    video_to_frames.py improvements

--- python/video_to_frames.py
+++ python/video_to_frames.py
@@ -1,30 +1,34 @@
 import cv2
 import glob
+import os
 
-videos = glob.glob("input*.mp4")
+path = "D:/images/draw3/videos/"
+videos = glob.glob(f"{path}*.mp4")
 name = 0
 test_txt = ""
 
 for video in videos:
+    video_name = os.path.basename(video).split('.', 1)[0]
     capture = cv2.VideoCapture(video)
     success, image = capture.read()
     count = 0
     frames_count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
     while success:
-        filename = "%04d.jpg" % name
+        filename = f"%04d-{video_name}.jpg" % name
         name += 1
-        cv2.imwrite("a/" + filename, image)  # save frame as JPEG file
+        cv2.imwrite(f"{path}../original/" + filename, image)
 
         row = ','.join(str(e) for e in [filename, -1, -1])
         test_txt += row + "\n"
 
         success, image = capture.read()
-        print('Read a new frame: ', success)
+        print("Read a new frame:", success)
 
-        count += 10
+        count += 20  # 60 FPS -> keep 3 frames per second
         capture.set(cv2.CAP_PROP_POS_FRAMES, count)
         if frames_count < count:
             success = False
+    capture.release()
 
-with open("test.txt", "w") as file:
+with open(f"{path}data.txt", "w") as file:
     file.write(test_txt)
